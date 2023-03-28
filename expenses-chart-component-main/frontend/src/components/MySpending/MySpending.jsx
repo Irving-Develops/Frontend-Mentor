512f49c24a3476cf1a38dda496200e912d70a02f
Irving Arreola Palacios <irving.a789@gmail.com>
2023-03-27 22:09:16 -0500

    finished

--- expenses-chart-component-main/frontend/src/components/MySpending/MySpending.jsx
+++ expenses-chart-component-main/frontend/src/components/MySpending/MySpending.jsx
@@ -23,21 +23,32 @@ export default function MySpending() {
                     // console.log(graphBar, "graphbar")
                     return (
                         <div key={idx} className={styles.graph}>
-                            {isActive ?
-                            <div className={styles.amountSpent}>{day.amount}</div>
-                            :
-                            <></>
-                        }
+  
                             <div onClick={() => handleClick(idx)} className={styles[`${graphBar}`]} style={{
                                 height: `${day.amount / total * 100 * 2}%`,
                             }} >
+                                {isActive ?
+                                    <div className={styles.amountSpent}>${day.amount}</div>
+                                    :
+                                    <></>
+                                }
                             </div>
-                            <p>
+                            <p className={styles.text}>
                                 {day.day}
                             </p>
                         </div>
                     )
                 })}
+            </div>
+            <div className={styles.cardBottom}>
+                <div className={styles.left}>
+                    <p style={{textAlign: "left"}}className={styles.text}>Total this month</p>
+                    <p style={{fontSize: "2rem"}}className={styles.title}>${total}</p>
+                </div>
+                <div className={styles.right}>
+                    <p style={{ textAlign: "right", color: "#392316", fontSize: "14px", fontWeight: "bold" }} className={styles.text}>+2.4%</p>
+                    <p style={{ textAlign: "right" }} className={styles.text}>from last month</p>
+                </div>
             </div>
         </div>
     )
