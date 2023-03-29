2009575842d2370cb77ecbe815ac0b5ba8720b46
Irving Arreola Palacios <irving.a789@gmail.com>
2023-03-28 23:10:13 -0500

    removed all unnecessary components

--- contact-form/frontend/src/Components/ContactForm/ContactForm.jsx
+++ contact-form/frontend/src/Components/ContactForm/ContactForm.jsx
@@ -1,46 +1,57 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 import styles from "./ContactForm.module.css";
-import {useScript} from "../useScript/useScript";
+import useScript from "../useScript/useScript";
+import emailjs from '@emailjs/browser'
 
 export default function ContactForm() {
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
+    const form = useRef();
 
 
-    useScript("https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js")
+
+    const handleSubmission = (e) => {
+        e.preventDefault();
 
 
+            emailjs.sendForm('service_fmxh477', 'contact_form', form.current, "CCmiUEjc9XRCALe-J")
+            .then((result) => {
+            console.log(result.text);
+            alert('Your message has been sent successfully!');
+            }, (error) => {
+            console.log(error.text);
+            alert('Sorry, an error occurred while sending your message.');
+            });
+
+        e.target.reset();
+    }
 
     return (
-        <form>
+        <form ref={form} onSubmit={handleSubmission}>
             <h1>Get In Touch</h1>
             <div>
                 <label htmlFor="First Name">First Name</label>
-                <input type="text" name="First Name" 
-                onChange={(e) => setFirstName(e.target.value)} 
+                <input type="text" name="first_name" 
+                // onChange={(e) => setFirstName(e.target.value)} 
                 />
             </div>
             <div>
                 <label htmlFor="Last Name">Last Name</label>
-                <input type="text" name="Last Name" 
-                onChange={(e) => setLastName(e.target.value)}
+                <input type="text" name="last_name" 
+                // onChange={(e) => setLastName(e.target.value)}
                 />
             </div>
             <div>
                 <label htmlFor="Email">Email</label>
-                <input type="text" name="Email" 
-                onChange={(e) => setEmail(e.target.value)}
+                <input type="text" name="email" 
+                // onChange={(e) => setEmail(e.target.value)}
                 />
             </div>
             <div>
                 <label htmlFor="Message">Message</label>
-                <input type="text" name="Message" 
-                onChange={(e) => setMessage(e.target.value)}
+                <input type="text" name="message" 
+                // onChange={(e) => setMessage(e.target.value)}
                 />
             </div>
-            <button type="submit">Submit</button>
+            <button type="submit" value="send">Submit</button>
             <p>Thanks for submitting!</p>
         </form>
     );
