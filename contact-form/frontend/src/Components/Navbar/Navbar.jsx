71937017c97e437780ff6b41114410230b6fd9d5
Irving Arreola Palacios <irving.a789@gmail.com>
2023-03-30 18:20:32 -0500

    navbar refactored

--- contact-form/frontend/src/Components/Navbar/Navbar.jsx
+++ contact-form/frontend/src/Components/Navbar/Navbar.jsx
@@ -10,21 +10,22 @@ export default function Navbar() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
                 </svg>  
             </div>
-            <div className="pr-8 md:block hidden text-secondaryText">
-                <ul>
+            <div className="pr-8 md:block hidden text-white w-3/5">
+                <ul className="flex items-center">
                     {navLinks.map((link, index) => (
                         (link === "More" ? 
-                            <li className="inline" key={index}>
-                                <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-secondaryText hover:text-white mr-4 group">
+                            <li className="" key={index}>
+                                <a href="#" className="block lg:inline-block text-white hover:text-secondaryText p-4 group transition-colors duration-300 ">
                                     {link}
-                                    <div class="absolute z-10 hidden group-hover:block bg-gray-800 rounded-md py-2 mt-2">
-                                    <a href="#" class="block px-4 py-2 text-sm text-gray-300 hover:text-white">Gallery</a>
-                                    <a href="#" class="block px-4 py-2 text-sm text-gray-300 hover:text-white">Breed Info</a>
+                                    <div className="absolute z-10 hidden group-hover:block bg-gray-800 rounded-md py-2 mt-2">
+                                    <a href="#" className="block px-4 py-2 text-sm text-gray-300 hover:text-white">Gallery</a>
+                                    <a href="#" className="block px-4 py-2 text-sm text-gray-300 hover:text-white">Breed Info</a>
+                                    <a href="#" className="block px-4 py-2 text-sm text-gray-300 hover:text-white">Language</a>
                                     </div>
                                 </a>
                             </li>
                             :
-                            <li className="inline" key={index}><a className="p-4" href={`/${link}`}>{link}</a></li>
+                            <li className="" key={index}><a className="hover:text-secondaryText transition-colors duration-300 p-4" href={`/${link}`}>{link}</a></li>
                         )
                         ))}
                 </ul>
