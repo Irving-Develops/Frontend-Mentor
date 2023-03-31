acf2d96ea187decc50b5d862cb235f14947806c2
Irving Arreola Palacios <irving.a789@gmail.com>
2023-03-31 14:16:32 -0500

    styling side panel

--- contact-form/frontend/src/Components/Navbar/Navbar.jsx
+++ contact-form/frontend/src/Components/Navbar/Navbar.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import { NavLink } from 'react-router-dom';
 import SetLanguage from "../SetLanguage/SetLanguage";
 import styles from "./Navbar.module.css";
-import DropdownModal from "./DropdownModal";
 
 
 export default function Navbar() {
     const navLinks = ["Home", "About", "Contact", "Parents", "Puppies", "More"];
+    const sidepanelLinks = ["Home", "About", "Contact", "Parents", "Puppies", "Gallery", "Breed Info"]
 
     const [open, setOpen] = useState(false);
 
@@ -24,20 +24,20 @@ export default function Navbar() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
                 </svg>  
             </div>
+            {/* This is the modal that appears when the hamburger menu is clicked */}
             <div className={`${styles.modal} ${open ? styles.transition : ''}`}>
                 <div className={styles.modalBackground} onClick={handleClick} />
                 <div className={`${styles.modalContent} ${open ? styles.transition : ''}`}>
-                    <ul className="">
-                    {navLinks.map((link, index) => (
+                    <ul className={styles.list}>
+                    {sidepanelLinks.map((link, index) => (
                         <li className="text-white" key={index}>
-                        <NavLink className="" to={`/${link}`}>
+                        <NavLink className="" to={`/${link}`} onClick={handleClick}>
                             {link}
                         </NavLink>
                         </li>
                     ))}
                     </ul>
                 </div>
-
             </div>
 
             <div className="hidden md:block">
