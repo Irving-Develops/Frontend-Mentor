db46563ced002743173e6667ca60e59f7f8a7a76
Irving Arreola Palacios <irving.a789@gmail.com>
2023-04-06 23:56:19 -0500

    added test button

--- contact-form/frontend/src/Pages/Hero/Hero.jsx
+++ contact-form/frontend/src/Pages/Hero/Hero.jsx
@@ -10,6 +10,7 @@ export default function Hero() {
                 <h1 className="text-2xl md:text-5xl lg:text-6xl font-bold mb-4 mt-8">Unlock the bond of a lifetime!</h1>
                 <p className="text-lg md:text-3xl lg:text-4xl mb-8">Discover the joy of owning a border collie today</p>
                 <Link to='/puppies' className='md:order-4 w-48 md:w-48 text-white bg-gold text-darkGrey font-medium py-2 px-4 rounded-md hover:bg-darkGold focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-darkGold'>View Puppies!</Link>
+                <button> Test</button>
             </div>
         </div>
     )
